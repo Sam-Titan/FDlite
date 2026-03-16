cd9a78ed91e4c840c5dadb89a62f20b1bfab56a3
Sam-Titan <bansalswayam2005@gmail.com>
2026-03-16 15:46:25 +0530

    Removing the base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/FDlite/',   // 👈 add this line — must match your repo name exactly
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: { '@': path.resolve(__dirname, '.') },
